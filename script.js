224dd7a826e71913c980699e46684243d2f69e1b
Kirill <kaarpusa2012@gmail.com>
2023-05-07 16:13:51 +0400

    Задание номер 2 выполнено

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-/* Добавил переменные  */
-const title = 2;
-let screens = 3;
-let screenPrice = 5;
-const rollback = 6;
-const fullPrice = 7;
-let adaptive = 15;
+let title = "Название проекта";
+console.log(typeof title);
+let screens = "Простые, Сложные, Интерактивные";
+console.log(screens.length);
+let screenPrice = 2000;
+console.log("Стоимость верстки экранов ${screenPrice} рублей/долларов/гривен/юани");
+let rollback = 10;
+let fullPrice = 30000;
+console.log(fullPrice * (rollback/100));
+console.log(typeof fullPrice);
+console.log("Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани");
+let adaptive = true;
+console.log(typeof adaptive);
 
-/*Вывод в консоль сообщения */
-
-console.log("Hello world!");
-
-/*Вывод на экран alert сообщение */
-
-alert("Hello World!!!!");
+let screensArray = screens.toLowerCase().split(", ");
+console.log(screensArray);
