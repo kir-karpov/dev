1f9d774e13ccfd3c951d7a4f20e02e73e46b0f1e
Kirill <kaarpusa2012@gmail.com>
2023-05-09 16:48:13 +0400

    Задание 3

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-let title = "Название проекта";
+`use strict`
+
+const service1 = prompt ("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = prompt ("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = prompt("Сколько это будет стоить ?");
+
+
+const title = prompt("Как называется ваш проект?");
 console.log(typeof title);
-let screens = "Простые, Сложные, Интерактивные";
+const screens = prompt("Какие типы экранов нужно разработать?");
 console.log(screens.length);
-let screenPrice = 2000;
-console.log("Стоимость верстки экранов ${screenPrice} рублей/долларов/гривен/юани");
-let rollback = 10;
-let fullPrice = 30000;
+const screenPrice = prompt ("Сколько будет стоить данная работа?");
+console.log("Стоимость верстки экранов ${screenPrice}` рублей");
+const rollback = 10;
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
 console.log(fullPrice * (rollback/100));
 console.log(typeof fullPrice);
-console.log("Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани");
-let adaptive = true;
+console.log("Стоимость разработки сайта ${fullPrice} рублей");
+const adaptive = confirm("Нужен ли адаптив на сайте?");
 console.log(typeof adaptive);
 
-let screensArray = screens.toLowerCase().split(", ");
+const screensArray = screens.toLowerCase().split(", ");
 console.log(screensArray);
+
+// задание 9)
+
+const Mediator = 0.3; // откат посреднику в 30%
+const servicePercentPrice = Math.ceil(fullPrice * (3 - Mediator));
+console.log("Стоимость работы с учетом коммисии посреднику" + servicePercentPrice + " руб.");
+
+// конструкция задания 10)
+
+if (fullPrice >= 30000) {
+  console.log("Даем скидку в 10%");
+} else if (fullPrice >= 15000 && fullPrice < 30000 ) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice < 15000 && fullPrice > 0 ) {
+  console.log("Скидка не предусмотрена");
+} else if ( fullPrice < 0 ) {
+  console.log("Что-то пошло не так");
+} 
